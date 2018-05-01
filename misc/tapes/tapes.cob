4cbd21b972cf936613f890b2c0e085685c8c2103
Mike DeRuiter <normalman54321@gmail.com>
2018-05-01 15:10:04 -0500

    tapes.cob: Polished

--- misc/tapes/tapes.cob
+++ misc/tapes/tapes.cob
@@ -1,5 +1,6 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. TAPE-COLLECTION.
+       AUTHOR. MIKE DERUITER.
        
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
@@ -7,28 +8,28 @@
        
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-            SELECT IP-TAPE
+            SELECT IP-TAPES
                 ASSIGN TO "/tmp/TAPES.DAT"
                 ORGANIZATION IS LINE SEQUENTIAL.
             SELECT OP-COLLECTION
                 ASSIGN TO "/tmp/tape-collection"
                 ORGANIZATION IS LINE SEQUENTIAL.
-            SELECT SW-TAPE
+            SELECT SW-TAPES
                 ORGANIZATION IS LINE SEQUENTIAL.
 
        DATA DIVISION.
         
        FILE SECTION.
-       FD  IP-TAPE.
-       01  IP-TAPE-REC              PIC X(52).
+       FD  IP-TAPES.
+       01  IP-TAPES-REC              PIC X(52).
 
-       SD  SW-TAPE.
-       01  SW-TAPE-REC.
-            05  SW-TAPE-ARTIST      PIC X(20).
-            05  SW-TAPE-TITLE       PIC X(20).
-            05  SW-TAPE-YEAR        PIC X(04).
-            05  SW-TAPE-VALUE       PIC 999V99.
-            05  SW-TAPE-RATING      PIC 9V99.
+       SD  SW-TAPES.
+       01  SW-TAPES-REC.
+            05  SW-TAPES-ARTIST      PIC X(20).
+            05  SW-TAPES-TITLE       PIC X(20).
+            05  SW-TAPES-YEAR        PIC X(04).
+            05  SW-TAPES-VALUE       PIC 999V99.
+            05  SW-TAPES-RATING      PIC 9V99.
 
        FD  OP-COLLECTION.
        01  OP-COLLECTION-REC.              
@@ -45,16 +46,16 @@
 
        WORKING-STORAGE SECTION.
        01  WS-ACCUMULATORS.
-            05  A-TAPE-VALUE        PIC 9999V99 VALUE ZERO.
-            05  A-TAPE-RATING       PIC 999V99 VALUE ZERO.
-            05  A-TAPE-NUM          PIC 9999 VALUE ZERO.
+            05  A-TOTAL-TAPE-VALUE  PIC 9999V99 VALUE ZERO.
+            05  A-TOTAL-TAPE-RATING PIC 999V99 VALUE ZERO.
+            05  A-TOTAL-TAPE-NUM    PIC 9999 VALUE ZERO.
             05  A-YEAR-TAPE-VALUE   PIC 9999V99 VALUE ZERO.
             05  A-YEAR-TAPE-RATING  PIC 999V99 VALUE ZERO.
             05  A-YEAR-TAPE-NUM     PIC 9999 VALUE ZERO.
 
        01  WS-SWITCHES.
-            05  EOF-TAPE-SW         PIC X VALUE "N".
-                88  EOF-TAPE              VALUE "Y".            
+            05  EOF-IP-TAPES-SW         PIC X VALUE "N".
+                88  EOF-IP-TAPES              VALUE "Y".            
         
        01  WS-AVERAGES.
             05  WS-AVG-TAPE-RATE    PIC 999V99 VALUE ZERO.
@@ -97,7 +98,7 @@
             
        01  TITLE-1.
             05                      PIC X(28) VALUE ALL SPACES.
-            05                      PIC X(15) VALUE "MIKE'S TAPE ".
+            05                      PIC X(12) VALUE "MIKE'S TAPE ".
             05                      PIC X(10) VALUE "COLLECTION".
 
        01  HEADER-1.
@@ -137,11 +138,11 @@
        PROCEDURE DIVISION.
 
        00000-MAIN.
-            SORT SW-TAPE
-                ASCENDING SW-TAPE-YEAR,
-                          SW-TAPE-ARTIST,
-                          SW-TAPE-TITLE
-                USING IP-TAPE
+            SORT SW-TAPES
+                ASCENDING SW-TAPES-YEAR,
+                          SW-TAPES-ARTIST,
+                          SW-TAPES-TITLE
+                USING IP-TAPES
                 OUTPUT PROCEDURE OA1000-MAINLINE.
 
             STOP RUN.
@@ -149,7 +150,7 @@
        OA1000-MAINLINE.
             PERFORM OB1000-SETUP.
             PERFORM OB2000-PROCESS
-                UNTIL EOF-TAPE.
+                UNTIL EOF-IP-TAPES.
             PERFORM OB3000-WRAPUP.
 
        OB1000-SETUP.
@@ -163,52 +164,47 @@
 
             MOVE SPACES TO OP-COLLECTION-REC.
 
-            RETURN SW-TAPE
-                AT END MOVE "Y" TO EOF-TAPE-SW.
+            RETURN SW-TAPES
+                AT END MOVE "Y" TO EOF-IP-TAPES-SW.
                 
-            MOVE SW-TAPE-YEAR       TO WS-PREV-YEAR.
-            MOVE SW-TAPE-YEAR       TO OP-TAPE-YEAR.
+            MOVE SW-TAPES-YEAR              TO WS-PREV-YEAR.
+            MOVE SW-TAPES-YEAR              TO OP-TAPE-YEAR.
 
        OB2000-PROCESS.
-            IF SW-TAPE-YEAR IS NOT EQUAL TO WS-PREV-YEAR
+            IF SW-TAPES-YEAR IS NOT EQUAL TO WS-PREV-YEAR
                 PERFORM OC2000-PRINT-PAGE-TOTAL-AVG
-                MOVE SW-TAPE-YEAR   TO WS-PREV-YEAR
-                MOVE SW-TAPE-YEAR   TO OP-TAPE-YEAR
+                MOVE SW-TAPES-YEAR          TO WS-PREV-YEAR
+                MOVE SW-TAPES-YEAR          TO OP-TAPE-YEAR
             END-IF.
        
-            MOVE SW-TAPE-TITLE      TO OP-TAPE-TITLE.
-            MOVE SW-TAPE-ARTIST     TO OP-TAPE-ARTIST.
-            MOVE SW-TAPE-VALUE      TO OP-TAPE-VALUE.
-            MOVE SW-TAPE-RATING     TO OP-TAPE-RATING.
+            MOVE SW-TAPES-TITLE             TO OP-TAPE-TITLE.
+            MOVE SW-TAPES-ARTIST            TO OP-TAPE-ARTIST.
+            MOVE SW-TAPES-VALUE             TO OP-TAPE-VALUE.
+            MOVE SW-TAPES-RATING            TO OP-TAPE-RATING.
 
-            ADD  SW-TAPE-VALUE      TO A-TAPE-VALUE.
-            ADD  SW-TAPE-RATING     TO A-TAPE-RATING.
-            ADD  1                  TO A-TAPE-NUM.
-
-            ADD  SW-TAPE-VALUE      TO A-YEAR-TAPE-VALUE.
-            ADD  SW-TAPE-RATING     TO A-YEAR-TAPE-RATING.
-            ADD  1                  TO A-YEAR-TAPE-NUM.
+            ADD  SW-TAPES-VALUE             TO A-YEAR-TAPE-VALUE.
+            ADD  SW-TAPES-RATING            TO A-YEAR-TAPE-RATING.
+            ADD  1                          TO A-YEAR-TAPE-NUM.
 
             WRITE OP-COLLECTION-REC
                 AFTER ADVANCING 1 LINE.
 
-            MOVE SPACES             TO OP-TAPE-YEAR.
+            MOVE SPACES                     TO OP-TAPE-YEAR.
 
-            RETURN SW-TAPE
-                AT END MOVE "Y" TO EOF-TAPE-SW.
+            RETURN SW-TAPES
+                AT END MOVE "Y" TO EOF-IP-TAPES-SW.
 
        OB3000-WRAPUP.
             PERFORM OC2000-PRINT-PAGE-TOTAL-AVG
             PERFORM OC3000-PRINT-GRAND-TOTAL-AVG.
             
             CLOSE OP-COLLECTION.
-
                 
        OC2000-PRINT-PAGE-TOTAL-AVG.
             PERFORM OD1000-CALC-YEAR-TOTAL-AVG.
        
-            MOVE WS-PREV-YEAR       TO YA-YEAR.
-            MOVE WS-PREV-YEAR       TO YT-YEAR.
+            MOVE WS-PREV-YEAR               TO YA-YEAR.
+            MOVE WS-PREV-YEAR               TO YT-YEAR.
             
             WRITE OP-COLLECTION-REC FROM FOOTER-DASHES
                 AFTER ADVANCING 1 LINE.
@@ -217,11 +213,7 @@
             WRITE OP-COLLECTION-REC FROM YEAR-TOTAL-REC
                 AFTER ADVANCING 1 LINE.
                 
-            MOVE ZEROES             TO A-YEAR-TAPE-VALUE.
-            MOVE ZEROES             TO A-YEAR-TAPE-RATING.
-            MOVE ZEROES             TO A-YEAR-TAPE-NUM.
-                
-            MOVE SPACES             TO OP-COLLECTION-REC.
+            MOVE SPACES                     TO OP-COLLECTION-REC.
             
             WRITE OP-COLLECTION-REC
                 AFTER ADVANCING 1 LINE.
@@ -237,25 +229,36 @@
                 AFTER ADVANCING 2 LINES.
             WRITE OP-COLLECTION-REC FROM GRAND-TOTAL-REC
                 AFTER ADVANCING 1 LINE.
+
+            MOVE SPACES                     TO OP-COLLECTION-REC.
                 
        OD1000-CALC-YEAR-TOTAL-AVG.
-            MOVE A-YEAR-TAPE-VALUE  TO YT-TAPE-VALUE.
+            MOVE A-YEAR-TAPE-VALUE          TO YT-TAPE-VALUE.
             
-            MOVE A-YEAR-TAPE-VALUE  TO WS-AVG-TAPE-VAL.
-            MOVE A-YEAR-TAPE-RATING TO WS-AVG-TAPE-RATE.
+            MOVE A-YEAR-TAPE-VALUE          TO WS-AVG-TAPE-VAL.
+            MOVE A-YEAR-TAPE-RATING         TO WS-AVG-TAPE-RATE.
             
             DIVIDE A-YEAR-TAPE-NUM INTO WS-AVG-TAPE-VAL
                 GIVING YA-TAPE-VALUE.
             DIVIDE A-YEAR-TAPE-NUM INTO WS-AVG-TAPE-RATE
                 GIVING YA-TAPE-RATING.
                 
+            ADD  A-YEAR-TAPE-VALUE          TO A-TOTAL-TAPE-VALUE.
+            ADD  A-YEAR-TAPE-RATING         TO A-TOTAL-TAPE-RATING.
+            ADD  1                          TO A-TOTAL-TAPE-NUM.
+                
+            MOVE ZEROES                     TO A-YEAR-TAPE-VALUE.
+            MOVE ZEROES                     TO A-YEAR-TAPE-RATING.
+            MOVE ZEROES                     TO A-YEAR-TAPE-NUM.
+
+                
        OD2000-CALC-GRAND-TOTAL-AVG.
-            MOVE A-TAPE-VALUE       TO GT-TAPE-VALUE.
+            MOVE A-TOTAL-TAPE-VALUE         TO GT-TAPE-VALUE.
             
-            MOVE A-TAPE-VALUE       TO WS-AVG-TAPE-VAL.
-            MOVE A-TAPE-RATING      TO WS-AVG-TAPE-RATE.
+            MOVE A-TOTAL-TAPE-VALUE         TO WS-AVG-TAPE-VAL.
+            MOVE A-TOTAL-TAPE-RATING        TO WS-AVG-TAPE-RATE.
             
-            DIVIDE A-TAPE-NUM INTO WS-AVG-TAPE-VAL
+            DIVIDE A-TOTAL-TAPE-NUM INTO WS-AVG-TAPE-VAL
                 GIVING GA-TAPE-VALUE.
-            DIVIDE A-TAPE-NUM INTO WS-AVG-TAPE-RATE
+            DIVIDE A-TOTAL-TAPE-NUM INTO WS-AVG-TAPE-RATE
                 GIVING GA-TAPE-RATING.
