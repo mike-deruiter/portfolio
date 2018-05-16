a8095b84f1ce97fc105da5571f8d3c4d762205c9
Mike DeRuiter <normalman54321@gmail.com>
2018-05-16 16:28:28 -0500

    music.cob: Added best/worst years

--- misc/music.d/music.cob
+++ misc/music.d/music.cob
@@ -1,5 +1,5 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. TAPE-COLLECTION.
+       PROGRAM-ID. MUSIC-COLLECTION.
        AUTHOR. MIKE DERUITER.
        
        ENVIRONMENT DIVISION.
@@ -8,58 +8,62 @@
        
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-            SELECT IP-TAPES
-                ASSIGN TO "/tmp/TAPES.DAT"
+            SELECT IP-MUSIC
+                ASSIGN TO "/tmp/MUSIC.DAT"
                 ORGANIZATION IS LINE SEQUENTIAL.
             SELECT OP-COLLECTION
                 ASSIGN TO DISPLAY
                 ORGANIZATION IS LINE SEQUENTIAL.
-            SELECT SW-TAPES
+            SELECT SW-MUSIC
                 ORGANIZATION IS LINE SEQUENTIAL.
 
        DATA DIVISION.
         
        FILE SECTION.
-       FD  IP-TAPES.
-       01  IP-TAPES-REC                 PIC X(52).
+       FD  IP-MUSIC.
+       01  IP-MUSIC-REC                 PIC X(52).
 
-       SD  SW-TAPES.
-       01  SW-TAPES-REC.
-            05  SW-TAPES-ARTIST         PIC X(20).
-            05  SW-TAPES-TITLE          PIC X(20).
-            05  SW-TAPES-YEAR           PIC X(04).
-            05  SW-TAPES-VALUE          PIC 999V99.
-            05  SW-TAPES-RATING         PIC 9V99.
+       SD  SW-MUSIC.
+       01  SW-MUSIC-REC.
+            05  SW-MUSIC-ARTIST         PIC X(20).
+            05  SW-MUSIC-TITLE          PIC X(20).
+            05  SW-MUSIC-YEAR           PIC X(04).
+            05  SW-MUSIC-VALUE          PIC 999V99.
+            05  SW-MUSIC-RATING         PIC 9V99.
 
        FD  OP-COLLECTION.
        01  OP-COLLECTION-REC.              
             05                          PIC X(06).
-            05  OP-TAPE-YEAR            PIC X(04).
+            05  OP-MUSIC-YEAR            PIC X(04).
             05                          PIC X(03).
-            05  OP-TAPE-ARTIST          PIC X(20).
+            05  OP-MUSIC-ARTIST          PIC X(20).
             05                          PIC X(02).
-            05  OP-TAPE-TITLE           PIC X(20).
+            05  OP-MUSIC-TITLE           PIC X(20).
             05                          PIC X(03).
-            05  OP-TAPE-VALUE           PIC $$$9.99.
+            05  OP-MUSIC-VALUE           PIC $$$9.99.
             05                          PIC X(05).
-            05  OP-TAPE-RATING          PIC 9.99.
+            05  OP-MUSIC-RATING          PIC 9.99.
 
        WORKING-STORAGE SECTION.
        01  WS-ACCUMULATORS.
-            05  A-TOTAL-TAPE-VALUE      PIC 9999V99 VALUE ZERO.
-            05  A-TOTAL-TAPE-RATING     PIC 999V99 VALUE ZERO.
-            05  A-TOTAL-TAPE-NUM        PIC 9999 VALUE ZERO.
-            05  A-YEAR-TAPE-VALUE       PIC 9999V99 VALUE ZERO.
-            05  A-YEAR-TAPE-RATING      PIC 999V99 VALUE ZERO.
-            05  A-YEAR-TAPE-NUM         PIC 9999 VALUE ZERO.
+            05  A-TOTAL-MUSIC-VALUE      PIC 9999V99 VALUE ZERO.
+            05  A-TOTAL-MUSIC-RATING     PIC 999V99 VALUE ZERO.
+            05  A-TOTAL-MUSIC-NUM        PIC 9999 VALUE ZERO.
+            05  A-YEAR-MUSIC-VALUE       PIC 9999V99 VALUE ZERO.
+            05  A-YEAR-MUSIC-RATING      PIC 999V99 VALUE ZERO.
+            05  A-YEAR-MUSIC-NUM         PIC 9999 VALUE ZERO.
 
        01  WS-SWITCHES.
-            05  EOF-IP-TAPES-SW         PIC X VALUE "N".
-                88  EOF-IP-TAPES              VALUE "Y".            
+            05  EOF-IP-MUSIC-SW         PIC X VALUE "N".
+                88  EOF-IP-MUSIC              VALUE "Y".            
         
        01  WS-AVERAGES.
-            05  WS-AVG-TAPE-RATING      PIC 999V99 VALUE ZERO.
-            05  WS-AVG-TAPE-VAL         PIC 9999V99 VALUE ZERO.
+            05  WS-AVG-MUSIC-RATING      PIC 999V99 VALUE ZERO.
+            05  WS-AVG-MUSIC-VAL         PIC 9999V99 VALUE ZERO.
+        
+       01  WS-HIGHEST-LOWEST.
+            05  WS-HIGHEST-RATING   PIC 999V99 VALUE ZERO.
+            05  WS-LOWEST-RATING    PIC 999V99 VALUE 5.
         
        01  WS-TEMPORARY.
             05  WS-PREV-YEAR            PIC X(04) VALUE ALL SPACES.
@@ -69,36 +73,48 @@
             05  YA-YEAR                 PIC X(05) VALUE ALL SPACES.
             05                          PIC X(08) VALUE "AVERAGE:".
             05                          PIC X(40) VALUE ALL SPACES.
-            05  YA-TAPE-VALUE           PIC $$9.99 VALUE ZERO.
+            05  YA-MUSIC-VALUE          PIC $$9.99 VALUE ZERO.
             05                          PIC X(05) VALUE ALL SPACES.
-            05  YA-TAPE-RATING          PIC 9.99 VALUE ZERO.
+            05  YA-MUSIC-RATING         PIC 9.99 VALUE ZERO.
             
        01  YEAR-TOTAL-REC.
             05                          PIC X(06) VALUE ALL SPACES.
             05  YT-YEAR                 PIC X(05) VALUE ALL SPACES.
             05                          PIC X(06) VALUE "TOTAL:".
             05                          PIC X(40) VALUE ALL SPACES.
-            05  YT-TAPE-VALUE           PIC $,$$9.99 VALUE ZERO.
+            05  YT-MUSIC-VALUE          PIC $,$$9.99 VALUE ZERO.
         
        01  GRAND-AVERAGE-REC.
             05                          PIC X(06) VALUE ALL SPACES.
             05                          PIC X(06) VALUE "GRAND ".
             05                          PIC X(08) VALUE "AVERAGE:".
             05                          PIC X(39) VALUE ALL SPACES.
-            05  GA-TAPE-VALUE           PIC $$9.99 VALUE ZERO.
+            05  GA-MUSIC-VALUE          PIC $$9.99 VALUE ZERO.
             05                          PIC X(05) VALUE ALL SPACES.
-            05  GA-TAPE-RATING          PIC 9.99 VALUE ZERO.
+            05  GA-MUSIC-RATING         PIC 9.99 VALUE ZERO.
 
        01  GRAND-TOTAL-REC.
             05                          PIC X(06) VALUE ALL SPACES.
             05                          PIC X(06) VALUE "GRAND ".
             05                          PIC X(06) VALUE "TOTAL:".
             05                          PIC X(39) VALUE ALL SPACES.
-            05  GT-TAPE-VALUE           PIC $,$$9.99 VALUE ZERO.
+            05  GT-MUSIC-VALUE          PIC $,$$9.99 VALUE ZERO.
+            
+       01  HIGHEST-YEAR-REC.
+            05                          PIC X(06) VALUE ALL SPACES.
+            05                          PIC X(11) VALUE "BEST YEAR: ".
+            05                          PIC X(53) VALUE SPACES.
+            05  HY-YEAR                 PIC X(04) VALUE SPACES.
+            
+       01  LOWEST-YEAR-REC.
+            05                          PIC X(06) VALUE ALL SPACES.
+            05                          PIC X(11) VALUE "WORST YEAR:".
+            05                          PIC X(53) VALUE SPACES.
+            05  LY-YEAR                 PIC X(04) VALUE SPACES.
             
        01  TITLE-1.
             05                          PIC X(28) VALUE ALL SPACES.
-            05                          PIC X(12) VALUE "MIKE'S TAPE ".
+            05                          PIC X(13) VALUE "MIKE'S MUSIC ".
             05                          PIC X(10) VALUE "COLLECTION".
 
        01  HEADER-1.
@@ -138,11 +154,11 @@
        PROCEDURE DIVISION.
 
        00000-MAIN.
-            SORT SW-TAPES
-                ASCENDING SW-TAPES-YEAR,
-                          SW-TAPES-ARTIST,
-                          SW-TAPES-TITLE
-                USING IP-TAPES
+            SORT SW-MUSIC
+                ASCENDING SW-MUSIC-YEAR,
+                          SW-MUSIC-ARTIST,
+                          SW-MUSIC-TITLE
+                USING IP-MUSIC
                 OUTPUT PROCEDURE OA1000-MAINLINE.
 
             STOP RUN.
@@ -150,7 +166,7 @@
        OA1000-MAINLINE.
             PERFORM OB1000-SETUP.
             PERFORM OB2000-PROCESS
-                UNTIL EOF-IP-TAPES.
+                UNTIL EOF-IP-MUSIC.
             PERFORM OB3000-WRAPUP.
 
        OB1000-SETUP.
@@ -167,35 +183,38 @@
 
             MOVE SPACES TO OP-COLLECTION-REC.
 
-            RETURN SW-TAPES
-                AT END MOVE "Y" TO EOF-IP-TAPES-SW.
-                
-            MOVE SW-TAPES-YEAR          TO WS-PREV-YEAR.
-            MOVE SW-TAPES-YEAR          TO OP-TAPE-YEAR.
+            RETURN SW-MUSIC
+                AT END MOVE "Y" TO EOF-IP-MUSIC-SW.
+            
+            MOVE SW-MUSIC-YEAR          TO WS-PREV-YEAR.
+            MOVE SW-MUSIC-YEAR          TO OP-MUSIC-YEAR.
+            
+            MOVE SW-MUSIC-YEAR          TO HY-YEAR.
+            MOVE SW-MUSIC-YEAR          TO LY-YEAR.
 
        OB2000-PROCESS.
-            IF SW-TAPES-YEAR IS NOT EQUAL TO WS-PREV-YEAR
+            IF SW-MUSIC-YEAR IS NOT EQUAL TO WS-PREV-YEAR
                 PERFORM OC2000-PRINT-PAGE-TOTAL-AVG
-                MOVE SW-TAPES-YEAR      TO WS-PREV-YEAR
-                MOVE SW-TAPES-YEAR      TO OP-TAPE-YEAR
+                MOVE SW-MUSIC-YEAR      TO WS-PREV-YEAR
+                MOVE SW-MUSIC-YEAR      TO OP-MUSIC-YEAR
             END-IF.
        
-            MOVE SW-TAPES-TITLE         TO OP-TAPE-TITLE.
-            MOVE SW-TAPES-ARTIST        TO OP-TAPE-ARTIST.
-            MOVE SW-TAPES-VALUE         TO OP-TAPE-VALUE.
-            MOVE SW-TAPES-RATING        TO OP-TAPE-RATING.
+            MOVE SW-MUSIC-TITLE         TO OP-MUSIC-TITLE.
+            MOVE SW-MUSIC-ARTIST        TO OP-MUSIC-ARTIST.
+            MOVE SW-MUSIC-VALUE         TO OP-MUSIC-VALUE.
+            MOVE SW-MUSIC-RATING        TO OP-MUSIC-RATING.
 
-            ADD  SW-TAPES-VALUE         TO A-YEAR-TAPE-VALUE.
-            ADD  SW-TAPES-RATING        TO A-YEAR-TAPE-RATING.
-            ADD  1                      TO A-YEAR-TAPE-NUM.
+            ADD  SW-MUSIC-VALUE         TO A-YEAR-MUSIC-VALUE.
+            ADD  SW-MUSIC-RATING        TO A-YEAR-MUSIC-RATING.
+            ADD  1                      TO A-YEAR-MUSIC-NUM.
 
             WRITE OP-COLLECTION-REC
                 BEFORE ADVANCING 1 LINE.
 
-            MOVE SPACES                 TO OP-TAPE-YEAR.
+            MOVE SPACES                 TO OP-MUSIC-YEAR.
 
-            RETURN SW-TAPES
-                AT END MOVE "Y" TO EOF-IP-TAPES-SW.
+            RETURN SW-MUSIC
+                AT END MOVE "Y" TO EOF-IP-MUSIC-SW.
 
        OB3000-WRAPUP.
             PERFORM OC2000-PRINT-PAGE-TOTAL-AVG
@@ -225,6 +244,10 @@
                 BEFORE ADVANCING 1 LINE.
             WRITE OP-COLLECTION-REC FROM GRAND-TOTAL-AVG-DASHES
                 BEFORE ADVANCING 2 LINES.
+            WRITE OP-COLLECTION-REC FROM HIGHEST-YEAR-REC
+                BEFORE ADVANCING 1 LINE.
+            WRITE OP-COLLECTION-REC FROM LOWEST-YEAR-REC
+                BEFORE ADVANCING 2 LINES.
             WRITE OP-COLLECTION-REC FROM GRAND-AVERAGE-REC
                 BEFORE ADVANCING 1 LINE.
             WRITE OP-COLLECTION-REC FROM GRAND-TOTAL-REC
@@ -233,32 +256,40 @@
             MOVE SPACES                 TO OP-COLLECTION-REC.
                 
        OD1000-CALC-YEAR-TOTAL-AVG.
-            MOVE A-YEAR-TAPE-VALUE      TO YT-TAPE-VALUE.
+            MOVE A-YEAR-MUSIC-VALUE      TO YT-MUSIC-VALUE.
             
-            MOVE A-YEAR-TAPE-VALUE      TO WS-AVG-TAPE-VAL.
-            MOVE A-YEAR-TAPE-RATING     TO WS-AVG-TAPE-RATING.
+            MOVE A-YEAR-MUSIC-VALUE      TO WS-AVG-MUSIC-VAL.
+            MOVE A-YEAR-MUSIC-RATING     TO WS-AVG-MUSIC-RATING.
             
-            DIVIDE A-YEAR-TAPE-NUM INTO WS-AVG-TAPE-VAL
-                GIVING YA-TAPE-VALUE.
-            DIVIDE A-YEAR-TAPE-NUM INTO WS-AVG-TAPE-RATING
-                GIVING YA-TAPE-RATING.
+            DIVIDE A-YEAR-MUSIC-NUM INTO WS-AVG-MUSIC-VAL
+                GIVING YA-MUSIC-VALUE.
+            DIVIDE A-YEAR-MUSIC-NUM INTO WS-AVG-MUSIC-RATING
+                GIVING YA-MUSIC-RATING.
                 
-            ADD  A-YEAR-TAPE-VALUE      TO A-TOTAL-TAPE-VALUE.
-            ADD  A-YEAR-TAPE-RATING     TO A-TOTAL-TAPE-RATING.
-            ADD  A-YEAR-TAPE-NUM        TO A-TOTAL-TAPE-NUM.    
+            IF YA-MUSIC-RATING > WS-HIGHEST-RATING
+                MOVE YA-MUSIC-RATING    TO WS-HIGHEST-RATING
+                MOVE SW-MUSIC-YEAR      TO HY-YEAR
+            ELSE IF YA-MUSIC-RATING < WS-LOWEST-RATING
+                MOVE YA-MUSIC-RATING    TO WS-LOWEST-RATING
+                MOVE SW-MUSIC-YEAR      TO LY-YEAR
+            END-IF.
                 
-            MOVE ZEROES                 TO A-YEAR-TAPE-VALUE.
-            MOVE ZEROES                 TO A-YEAR-TAPE-RATING.
-            MOVE ZEROES                 TO A-YEAR-TAPE-NUM.
+            ADD  A-YEAR-MUSIC-VALUE      TO A-TOTAL-MUSIC-VALUE.
+            ADD  A-YEAR-MUSIC-RATING     TO A-TOTAL-MUSIC-RATING.
+            ADD  A-YEAR-MUSIC-NUM        TO A-TOTAL-MUSIC-NUM.    
+                
+            MOVE ZEROES                 TO A-YEAR-MUSIC-VALUE.
+            MOVE ZEROES                 TO A-YEAR-MUSIC-RATING.
+            MOVE ZEROES                 TO A-YEAR-MUSIC-NUM.
 
                 
        OD2000-CALC-GRAND-TOTAL-AVG.
-            MOVE A-TOTAL-TAPE-VALUE TO GT-TAPE-VALUE.
+            MOVE A-TOTAL-MUSIC-VALUE TO GT-MUSIC-VALUE.
             
-            MOVE A-TOTAL-TAPE-VALUE     TO WS-AVG-TAPE-VAL.
-            MOVE A-TOTAL-TAPE-RATING    TO WS-AVG-TAPE-RATING.
+            MOVE A-TOTAL-MUSIC-VALUE     TO WS-AVG-MUSIC-VAL.
+            MOVE A-TOTAL-MUSIC-RATING    TO WS-AVG-MUSIC-RATING.
             
-            DIVIDE A-TOTAL-TAPE-NUM INTO WS-AVG-TAPE-VAL
-                GIVING GA-TAPE-VALUE.
-            DIVIDE A-TOTAL-TAPE-NUM INTO WS-AVG-TAPE-RATING
-                GIVING GA-TAPE-RATING.
+            DIVIDE A-TOTAL-MUSIC-NUM INTO WS-AVG-MUSIC-VAL
+                GIVING GA-MUSIC-VALUE.
+            DIVIDE A-TOTAL-MUSIC-NUM INTO WS-AVG-MUSIC-RATING
+                GIVING GA-MUSIC-RATING.
