505b470e4736cbdadcc67041ab4496658eaf0c3d
Mike DeRuiter <michaelalanderuiter@gmail.com>
2020-02-01 14:26:08 -0600

    quicksort.c: Separated stack functionality

--- main/algorithms/quicksort.c
+++ main/algorithms/quicksort.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "stack.h"
+
 #if __STDC_VERSION__ >= 199901L
 #include <stdbool.h>
 #else
@@ -14,18 +16,6 @@ typedef enum { false, true } bool;
 #define MAX_VAL 99
 #define MIN_VAL 1
 #define SPACING 2
-
-// stack data structure, functions, & variable
-typedef struct {
-    int *stack;
-    int top;
-} Stack;
-
-Stack *init_stack(int size);
-void push(Stack *s, int i);
-int pop(Stack *s);
-int peek(Stack *s);
-bool stack_empty(Stack *s);
 
 void init_rand_array(int a[], int n);
 void quicksort(int a[], int n);
@@ -40,37 +30,6 @@ int main()
     output_array(a, 1, N);
 
     return 0;
-}
-
-/* init_stack - create stack object */
-Stack *init_stack(int size) {
-    Stack *s;
-
-    s = malloc(sizeof(Stack));
-    s->stack = malloc(size * sizeof(int));
-    s->top = -1;
-
-    return s;
-}
-
-/* push - push a value onto the stack. */
-void push(Stack *s, int i) {
-    s->stack[++s->top] = i;
-}
-
-/* pop - pop a value from the stack. */
-int pop(Stack *s) {
-    return s->stack[s->top--];
-}
-
-/* peek - return the value on top of the stack. */
-int peek(Stack *s) {
-    return s->stack[s->top];
-}
-
-/* stack_empty - returns true if stack is empty */
-bool stack_empty(Stack *s) {
-    return s->top == -1 ? true : false;
 }
 
 /* init_rand_array - initialize random array of size n */
